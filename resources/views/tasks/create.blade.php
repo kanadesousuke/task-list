fb48f72f9b379fd97a0a493457c73c31d7c793fa
kanade.sousuke <kanade_sousuke@yahoo.co.jp>
2018-12-10 14:16:01 +0000

    task-list

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,16 +2,27 @@
 
 @section('content')
     <h1>メッセージ新規作成ページ</h1>
+    <body>
+        <div class="row">
+            <div class="col-xs-12"></div>
+            <div class="col-sm-offset-2 col-sm-8"></div>
+            <div class="col-md-offset-2 col-sm-8"></div>
+            <div class="col-lg-offset-3 col-sm-6"></div>
+        </div>
+        
+            {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
     
-    {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'タイトル:') !!}
-        {!! Form::text('status') !!}
-
+        <div class="form-group">
+            {!! Form::label('status', 'タイトル:') !!}
+            {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+        
+        <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+    
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+     </body>   
     {!! Form::close() !!}
 @endsection
